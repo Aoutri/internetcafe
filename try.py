b42ced6e9308d4e89701ba0773f0a88a47fc548c
Aoutri <tri36172@gmail.com>
2022-12-12 02:56:00 +0700

    update again

--- try.py
+++ try.py
@@ -32,8 +32,8 @@ def submitted():
         cost = time_play * co
         profit = bill - cost
         new_mark = {'username': [username.get()], 'check in date':[calIn], 'check in':[timeIn], 'check out date':[calOut], 'check out':[timeOut], 'fee':[bill], 'cost':[cost], 'profit':[profit]} 
-        mark_df = pd.DataFrame(new_mark) 
-        frame = pd.concat([frame,mark_df], ignore_index=True) 
+        mark_df = pd.DataFrame(new_mark)
+        frame = pd.concat([frame,mark_df], ignore_index=True)
         sum_profit = {'profit':[frame['profit'].sum()]}
         sum_profit = pd.DataFrame(sum_profit)
         frame = pd.concat([frame,sum_profit], ignore_index=True)
@@ -62,22 +62,21 @@ def submitted():
         submit_text.place(x=220, y=380)
         submit_text.after(3000, submit_text.destroy)
 
-    else: #Err
+    else: 
         submit_text = Label(window, text='error!!! wrong time input', fg='red', font=('Helvetica', 12))
         submit_text.place(x=220, y=380)
         submit_text.after(3000, submit_text.destroy)
         print('error!!! wrong time input')
 
-window = Tk() 
+window = Tk()
 
-#username
+
 username_title = Label(window, text='Username', fg='black', font=('Helvetica', 16))
 username_title.place(x=30, y=20)
 username = Entry(window, text='Enter your username')
 username.place(x=30, y=60)
 
-#time in
-#time out
+
 time = ['00.00', '01.00', '02.00', '03.00', '04.00', '05.00', '06.00', '07.00', '08.00', '09.00', '10.00', '11.00', '12.00', '13.00', '14.00', '15.00', '16.00', '17.00', '18.00', '19.00', '20.00', '21.00', '22.00', '23.00']
 
 time_title = Label(window, text='Log in - Log out', fg='black', font=('Helvetica', 16))
@@ -89,7 +88,7 @@ cal_in.place(x=30, y=150)
 cal_out = Calendar(window, selectmode = 'day', year = 2022, month = 12, day = 13)
 cal_out.place(x=300, y=150)
 
-time_in = Combobox(window, values=time) #ตัวที่เลื่อนลงมาเลือกได้
+time_in = Combobox(window, values=time)
 time_in.place(x=30, y=330)
 
 time_out = Combobox(window, values=time)
