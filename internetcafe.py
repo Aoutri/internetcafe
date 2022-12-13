cbb96af857681e80410291df9cc3e4120efffda8
Aoutri <tri36172@gmail.com>
2022-12-13 20:55:16 +0700

    update

--- internetcafe.py
+++ internetcafe.py
@@ -1,74 +1,172 @@
-from tkinter import * 
+from tkinter import *
 from tkinter.ttk import Combobox
 import pandas as pd
-import time as t
 from tkcalendar import Calendar
 
-fee = 20 
-co = 5 
+fee = 20 #ค่าบริการ
+co = 5 #ต้นทุน
 
-try: 
+try: #find xls from path
     path = 'C:\Python\python project\internetcafe/internetcafe.csv'
     frame = pd.DataFrame(pd.read_csv(path))
-except: 
+except: #create xls
     empty = {'username': [], 'check in date':[], 'check in':[], 'check out date':[] , 'check out':[], 'fee':[], 'cost':[], 'profit':[]}
     frame = pd.DataFrame(empty)
     frame.to_csv('C:\Python\python project\internetcafe/internetcafe.csv')
 
 def submitted():
-    timeInf = float(time[time_in.current()]) 
-    timeOutf = float(time[time_out.current()]) 
+    timeInf = float(time[time_in.current()]) #เก็บค่าเวลาเข้า
+    timeOutf = float(time[time_out.current()]) #เก็บค่าเวลาออก
     timeIn = int(timeInf)
-    timeOut = int(timeOutf) 
-    calIn = cal_in.get_date() 
-    calOut = cal_out.get_date() 
-    global frame 
+    timeOut = int(timeOutf) #con to int
+    calIn = cal_in.get_date() #get datein
+    calOut = cal_out.get_date() #get dateout
+    global frame #ประกาศตัวแปร
+    try:
+        monthIn = int(calIn[0:2])
+    except:
+        monthIn = int(calIn[0:1])
+    try:
+        monthOut = int(calOut[0:2])
+    except:
+        monthOut = int(calOut[0:1])
+    if monthIn < 10:
+        try:
+            dayIn = int(calIn[2:4])
+        except:
+            dayIn = int(calIn[2:3])
+    else:
+        try:
+            dayIn = int(calIn[3:5])
+        except:
+            dayIn = int(calIn[3:4])
+    if monthOut < 10:
+        try:
+            dayOut = int(calOut[2:4])
+        except:
+            dayOut = int(calOut[2:3])
+    else:
+        try:
+            dayOut = int(calOut[3:5])
+        except:
+            dayOut = int(calOut[3:4])
 
-    if timeOut > timeIn: 
-        if len(frame) != 0:
-            frame = frame.drop(len(frame)-1)
-        time_play = timeOut - timeIn
-        bill = time_play * fee
-        cost = time_play * co
-        profit = bill - cost
-        new_mark = {'username': [username.get()], 'check in date':[calIn], 'check in':[timeIn], 'check out date':[calOut], 'check out':[timeOut], 'fee':[bill], 'cost':[cost], 'profit':[profit]} 
-        mark_df = pd.DataFrame(new_mark) 
-        frame = pd.concat([frame,mark_df], ignore_index=True) 
-        sum_profit = {'profit':[frame['profit'].sum()]}
-        sum_profit = pd.DataFrame(sum_profit)
-        frame = pd.concat([frame,sum_profit], ignore_index=True)
-        frame.to_csv('C:\Python\python project\internetcafe/internetcafe.csv', index=False) 
+    if len(calIn) == 6:
+        yearIn = int(calIn[4:6])
+    elif len(calIn) == 7:
+        yearIn = int(calIn[5:7])
+    elif len(calIn) == 8:
+        yearIn = int(calIn[6:8])
+    if len(calOut) == 6:
+        yearOut = int(calOut[4:6])
+    elif len(calOut) == 7:
+        yearOut = int(calOut[5:7])
+    elif len(calOut) == 8:
+        yearOut = int(calOut[6:8])
 
-        submit_text = Label(window, text='Submitted', fg='red', font=('Helvetica', 12))
-        submit_text.place(x=220, y=380)
-        submit_text.after(3000, submit_text.destroy)
 
-    elif calIn != calOut: 
-        if len(frame) != 0:
-            frame = frame.drop(len(frame)-1)
-        time_play = (24 - timeIn) + timeOut
-        bill = time_play * fee
-        cost = time_play * co
-        profit = bill - cost
-        new_mark = {'username': [username.get()], 'check in date':[calIn], 'check in':[timeIn], 'check out date':[calOut], 'check out':[timeOut], 'fee':[bill], 'cost':[cost], 'profit':[profit]}
-        mark_df = pd.DataFrame(new_mark)
-        frame = pd.concat([frame,mark_df], ignore_index=True)
-        sum_profit = {'profit':[frame['profit'].sum()]}
-        sum_profit = pd.DataFrame(sum_profit)
-        frame = pd.concat([frame,sum_profit], ignore_index=True)
-        frame.to_csv('C:\Python\python project\internetcafe/internetcafe.csv', index=False)
-
-        submit_text = Label(window, text='Submitted', fg='red', font=('Helvetica', 12))
-        submit_text.place(x=220, y=380)
-        submit_text.after(3000, submit_text.destroy)
-
-    else: #Err
+    realDay = dayOut - dayIn
+    realMonth = abs(monthOut - monthIn)
+    realYear = abs(yearOut - yearIn)
+    if yearOut ==  yearIn and monthOut < monthIn:
         submit_text = Label(window, text='error!!! wrong time input', fg='red', font=('Helvetica', 12))
         submit_text.place(x=220, y=380)
         submit_text.after(3000, submit_text.destroy)
         print('error!!! wrong time input')
+    elif yearOut == yearIn and monthOut == monthIn and dayOut < dayIn:
+        submit_text = Label(window, text='error!!! wrong time input', fg='red', font=('Helvetica', 12))
+        submit_text.place(x=220, y=380)
+        submit_text.after(3000, submit_text.destroy)
+        print('error!!! wrong time input')
+    elif yearOut == yearIn and monthOut == monthIn and dayOut == dayIn and timeOut < timeIn:
+        submit_text = Label(window, text='error!!! wrong time input', fg='red', font=('Helvetica', 12))
+        submit_text.place(x=220, y=380)
+        submit_text.after(3000, submit_text.destroy)
+        print('error!!! wrong time input')
+    else:
+        if timeOut > timeIn: 
+            if len(frame) != 0:
+                frame = frame.drop(len(frame)-1)
+            time_play = timeOut - timeIn
+            bill = time_play * fee
+            cost = time_play * co
+            profit = bill - cost
+            new_mark = {'username': [username.get()], 'check in date':[calIn], 'check in':[timeIn], 'check out date':[calOut], 'check out':[timeOut], 'fee':[bill], 'cost':[cost], 'profit':[profit]} #xls
+            mark_df = pd.DataFrame(new_mark)
+            frame = pd.concat([frame,mark_df], ignore_index=True)
+            sum_profit = {'profit':[frame['profit'].sum()]}
+            sum_profit = pd.DataFrame(sum_profit)
+            frame = pd.concat([frame,sum_profit], ignore_index=True)
+            frame.to_csv('C:\Python\python project\internetcafe/internetcafe.csv', index=False) #save
 
-window = Tk() 
+            submit_text = Label(window, text='Submitted', fg='red', font=('Helvetica', 12))
+            submit_text.place(x=220, y=380)
+            submit_text.after(3000, submit_text.destroy)
+
+        elif yearIn != yearOut:
+            if len(frame) != 0:
+                frame = frame.drop(len(frame)-1)
+            time_play = ((realYear - 1) * 365 * 24) + (((12 - monthIn) + (monthOut - 1)) * 31 * 24) + (((31 - dayIn) + (dayOut - 1)) * 24) + ((24 - timeIn) + timeOut)
+            bill = time_play * fee
+            cost = time_play * co
+            profit = bill - cost
+            new_mark = {'username': [username.get()], 'check in date':[calIn], 'check in':[timeIn], 'check out date':[calOut], 'check out':[timeOut], 'fee':[bill], 'cost':[cost], 'profit':[profit]} #xls
+            mark_df = pd.DataFrame(new_mark)
+            frame = pd.concat([frame,mark_df], ignore_index=True)
+            sum_profit = {'profit':[frame['profit'].sum()]}
+            sum_profit = pd.DataFrame(sum_profit)
+            frame = pd.concat([frame,sum_profit], ignore_index=True)
+            frame.to_csv('C:\Python\python project\internetcafe/internetcafe.csv', index=False) #save
+
+            submit_text = Label(window, text='Submitted', fg='red', font=('Helvetica', 12))
+            submit_text.place(x=220, y=380)
+            submit_text.after(3000, submit_text.destroy)
+
+        elif monthIn != monthOut:
+            if len(frame) != 0:
+                frame = frame.drop(len(frame)-1)
+            time_play = ((realMonth - 1) * 31 * 24) + (((31 - dayIn) + (dayOut - 1)) * 24) + ((24 - timeIn) + timeOut)
+            bill = time_play * fee
+            cost = time_play * co
+            profit = bill - cost
+            new_mark = {'username': [username.get()], 'check in date':[calIn], 'check in':[timeIn], 'check out date':[calOut], 'check out':[timeOut], 'fee':[bill], 'cost':[cost], 'profit':[profit]} #xls
+            mark_df = pd.DataFrame(new_mark)
+            frame = pd.concat([frame,mark_df], ignore_index=True)
+            sum_profit = {'profit':[frame['profit'].sum()]}
+            sum_profit = pd.DataFrame(sum_profit)
+            frame = pd.concat([frame,sum_profit], ignore_index=True)
+            frame.to_csv('C:\Python\python project\internetcafe/internetcafe.csv', index=False) #save
+
+            submit_text = Label(window, text='Submitted', fg='red', font=('Helvetica', 12))
+            submit_text.place(x=220, y=380)
+            submit_text.after(3000, submit_text.destroy)
+            
+        elif dayIn != dayOut: #diff day
+            if len(frame) != 0:
+                frame = frame.drop(len(frame)-1)
+            time_play = ((24 - timeIn) + timeOut) + ((realDay - 1) * 24)
+            bill = time_play * fee
+            cost = time_play * co
+            profit = bill - cost
+            new_mark = {'username': [username.get()], 'check in date':[calIn], 'check in':[timeIn], 'check out date':[calOut], 'check out':[timeOut], 'fee':[bill], 'cost':[cost], 'profit':[profit]}
+            mark_df = pd.DataFrame(new_mark)
+            frame = pd.concat([frame,mark_df], ignore_index=True)
+            sum_profit = {'profit':[frame['profit'].sum()]}
+            sum_profit = pd.DataFrame(sum_profit)
+            frame = pd.concat([frame,sum_profit], ignore_index=True)
+            frame.to_csv('C:\Python\python project\internetcafe/internetcafe.csv', index=False)
+
+            submit_text = Label(window, text='Submitted', fg='red', font=('Helvetica', 12))
+            submit_text.place(x=220, y=380)
+            submit_text.after(3000, submit_text.destroy)
+
+        else: #Err
+            submit_text = Label(window, text='error!!! wrong time input', fg='red', font=('Helvetica', 12))
+            submit_text.place(x=220, y=380)
+            submit_text.after(3000, submit_text.destroy)
+            print('error!!! wrong time input')
+
+window = Tk()
 
 #username
 username_title = Label(window, text='Username', fg='black', font=('Helvetica', 16))
@@ -89,7 +187,7 @@ cal_in.place(x=30, y=150)
 cal_out = Calendar(window, selectmode = 'day', year = 2022, month = 12, day = 13)
 cal_out.place(x=300, y=150)
 
-time_in = Combobox(window, values=time) #ตัวที่เลื่อนลงมาเลือกได้
+time_in = Combobox(window, values=time)
 time_in.place(x=30, y=330)
 
 time_out = Combobox(window, values=time)
